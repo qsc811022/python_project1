f41ac0ef02eb17a917dccb4ef22a6f78c93b2e57
劉敦桐 <zse811022@hotmail.com>
2023-10-09 22:27:38 +0800

    123

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-#import antoher_module
-#print(antoher_module.another_variable)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-#from turtle import Turtle,Screen
-#timmy=Turtle()
-#print(timmy)
-#timmy.shape("turtle")
-#timmy.color("coral")
-#timmy.forward(100)
+money_machine=MoneyMachine()
+coffee_maker=CoffeeMaker()
+menu=Menu()
 
-#my_screen=Screen()
-#print(my_screen.canvwidth)
-#my_screen.exitonclick
+coffee_maker.report()
+money_machine.report()
 
-from prettytable import PrettyTable
+is_on=True
 
-table=PrettyTable()
-table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmander"])
-table.add_column("Type",["Electric","Water","Fire"])
-
-print(table)
+while is_on:
+    options=menu.get_items()
+    choice=input(f"What would you like? ({options}): ")
+    if choice=="off":
+        is_on=False
+    elif choice=="report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink=menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
